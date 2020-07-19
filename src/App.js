92d2ab5fc8faaf79c55fcc895f2ca7ec179386b1
jin <lj7812@gmail.com>
2020-07-19 18:04:04 +0900

    # 3.0 useEffect (done)

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ const App = () => {
     console.log("hello ~");
   };
 
-  useEffect(() => {
-    sayHello();
-  });
-
   const [number, setNumber] = useState(0);
   const [aNumber, setANumber] = useState(0);
+
+  // 1번째 인자는 실행할 function, 2번째 인자는 dependencies (예 : number state가 바뀔때만 function 실행)
+  // 이 예제에서 useEffect는 componentDidMount와 componentDidUpdate 인데, [number] state가 바뀔때만 동작
+  useEffect(sayHello, [number]);
 
   return (
     <div className="App">
