8576478927c50708b327c74311eecb2bab7b06ff
jin <lj7812@gmail.com>
2020-07-19 14:21:55 +0900

    # 2.0 useInput (done)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    // console.log(event);
-    const {
-      target: { value },
-    } = event;
-
-    let willUpdate = true;
-
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-
-    if (willUpdate) {
-      setValue(value);
-    }
-  };
-  return { value, onChange };
-};
-
 const App = () => {
-  // 아래와 동일 const maxLen = (value) => { return value.length < 10; };
-  const checkMaxLen = (value) => value.length < 10;
-  const checkValid = (value) => !value.includes("@");
-  const name = useInput("Mr.", checkValid);
-
-  // {...name} => name 안의 모든 것을 unpacking 한다. (ex. value, onChange, etc...)
   return (
     <div>
       <h1>Hello bro</h1>
-      <input placeholder="Name" {...name} />
     </div>
   );
 };
