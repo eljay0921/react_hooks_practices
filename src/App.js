e8a6cbc06de52173783edfae737eb0c2ed6781af
jin <lj7812@gmail.com>
2020-07-19 18:54:07 +0900

    # 3.2 useEffect 관련 js 정리

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useClick = (onClick) => {
-  // if (typeof onclick !== "function") {
-  //   return;
-  // }
-
-  const element = useRef();
-
-  useEffect(() => {
-    // 첫번째 인자가 componentDidMount 시 호출될 function
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
-    }
-
-    // useEffect에서 return하는 것은 : componentWillUnmount 시 호출되는 function
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-    // 두번째 인자(deps)는 dependencies (componentDidUpdate)
-  }, []);
-
-  return element;
-};
-
 const App = () => {
-  const sayHello = () => console.log("Hello bros");
-  const title = useClick(sayHello);
-
   return (
     <div className="App">
       <div>Hi !</div>
-      <h1 ref={title}>oh my god</h1>
     </div>
   );
 };
---
+++ src/useClick.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+
+export const useClick = (onClick) => {
+  // if (typeof onclick !== "function") {
+  //   return;
+  // }
+
+  const element = useRef();
+
+  useEffect(() => {
+    // 첫번째 인자가 componentDidMount 시 호출될 function
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+
+    // useEffect에서 return하는 것은 : componentWillUnmount 시 호출되는 function
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+    // 두번째 인자(deps)는 dependencies (componentDidUpdate)
+  }, []);
+
+  return element;
+};
