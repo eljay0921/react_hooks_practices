49ac296c1b73c4db20f204ee2a426b8427c75843
jin <lj7812@gmail.com>
2020-07-19 14:57:06 +0900

    # 3.0 useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  return <div className="App"></div>;
+  const sayHello = () => {
+    console.log("hello ~");
+  };
+
+  useEffect(() => {
+    sayHello();
+  });
+
+  const [number, setNumber] = useState(0);
+  const [aNumber, setANumber] = useState(0);
+
+  return (
+    <div className="App">
+      <div>Hi !</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
 };
 export default App;
