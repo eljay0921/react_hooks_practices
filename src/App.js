d22216d336b603ba22180258fcedfec01e0410a3
jin <lj7812@gmail.com>
2020-07-19 19:04:57 +0900

    # 3.3 useConfirm (not hook)
    Practices for functional programming

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect, useRef } from "react";
 
+const useConfirm = (message, onConfirm, onCancel) => {
+  if (!onConfirm && typeof onConfirm !== "function") {
+    return;
+  }
+
+  if (!onCancel && typeof onCancel !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    // eslint-disable-next-line no-restricted-globals
+    if (confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
+    }
+  };
+
+  return confirmAction;
+};
+
 const App = () => {
+  const abort = () => console.log("Nope.");
+  const deleteWorld = () => console.log("Deleting !!!!");
+  const confirmDelete = useConfirm("Are u sure ?", deleteWorld, abort);
+
   return (
     <div className="App">
       <div>Hi !</div>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 };
