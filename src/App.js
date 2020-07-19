bc895b5006d108e4788feebfaddcb0e659ebe6a1
jin <lj7812@gmail.com>
2020-07-19 18:26:57 +0900

    # 3.1 useTitle

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from "react";
 
-const App = () => {
-  const sayHello = () => {
-    console.log("hello ~");
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
   };
 
-  const [number, setNumber] = useState(0);
-  const [aNumber, setANumber] = useState(0);
+  useEffect(updateTitle, [title]);
 
-  // 1번째 인자는 실행할 function, 2번째 인자는 dependencies (예 : number state가 바뀔때만 function 실행)
-  // 이 예제에서 useEffect는 componentDidMount와 componentDidUpdate 인데, [number] state가 바뀔때만 동작
-  useEffect(sayHello, [number]);
+  return setTitle;
+};
 
+const App = () => {
+  const titleUpdater = useTitle("Loading.......");
+  setTimeout(() => titleUpdater("Home"), 3000);
   return (
     <div className="App">
       <div>Hi !</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setANumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
