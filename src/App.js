b1d81106784f17a5f2ba04e770b2b42e6ff9554e
jin <lj7812@gmail.com>
2020-07-19 14:44:08 +0900

    # 2.1 useTabs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
 
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    allTabs = content;
+  }
+
+  const [currentIdx, setCurrentIdx] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIdx],
+    changeItem: setCurrentIdx,
+  };
+};
+
 const App = () => {
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
-    <div>
-      <h1>Hello bro</h1>
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
