d16f2caec13dfeeaf9e8eb47aba8061eaefe571b
jin <lj7812@gmail.com>
2020-07-20 21:45:28 +0900

    # 3.3 useNetwork

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
+import useNetwork from "./useNetwork";
 
 const App = () => {
+  const handleNetworkChange = (status) =>
+    console.log(status ? "We ON-" : "We off...");
+  const onLine = useNetwork(handleNetworkChange);
+
   return (
     <div className="App">
       <h1>Hi u guys</h1>
+      <h1>{onLine ? "Oooooon line !" : "OFFline..."}</h1>
     </div>
   );
 };
