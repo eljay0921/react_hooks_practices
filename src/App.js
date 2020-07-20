115dd39b512f69adf95b81aab65576d7e0dc4389
jin <lj7812@gmail.com>
2020-07-20 20:37:11 +0900

    # 3.3 usePreventLeave (not hook - done)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
-  };
-
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload", listener);
-
-  return { enablePrevent, disablePrevent };
-};
-
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
   return (
     <div className="App">
       <div>Hi !</div>
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>Unprotect</button>
     </div>
   );
 };
