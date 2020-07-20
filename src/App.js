accefa5194c098eee8f5d480c848f70c77474baf
jin <lj7812@gmail.com>
2020-07-20 20:54:35 +0900

    # 3.3 useBeforeLeave (done)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import useBeaforeLeave from "./useBeforeLeave";
 
 const App = () => {
-  const begForLife = () => console.log("Don't leave!");
-  useBeaforeLeave(begForLife);
-
   return (
     <div className="App">
       <div>Hi !</div>
