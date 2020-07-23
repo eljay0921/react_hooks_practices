727f867b70ff561bd0aaca3dcc60abd602c5b1e1
jin <lj7812@gmail.com>
2020-07-23 20:12:31 +0900

    # 3.3 useNetwork (done)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import useNetwork from "./useNetwork";
 
 const App = () => {
-  const handleNetworkChange = (status) =>
-    console.log(status ? "We ON-" : "We off...");
-  const onLine = useNetwork(handleNetworkChange);
-
-  return (
-    <div className="App">
-      <h1>Hi u guys</h1>
-      <h1>{onLine ? "Oooooon line !" : "OFFline..."}</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 export default App;
