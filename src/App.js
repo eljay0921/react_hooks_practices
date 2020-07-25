30bd0c93fae9caf47cb6f314e8142f46911935db
jin <lj7812@gmail.com>
2020-07-25 14:50:02 +0900

    # 3.3 useNotification (done)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
-import useNotification from "./useNotification";
 
 const App = () => {
-  const notify = useNotification("Can I love You?", { body: "I Love You" });
-
-  return (
-    <div className="App">
-      <button onClick={notify}>Hello~!</button>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
