7ece2b99d46fad63502423a203a0de086e4f63a7
jin <lj7812@gmail.com>
2020-07-25 15:06:19 +0900

    # 3.3 useAxios (1)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
+import useAxios from "./useAxios";
 
 const App = () => {
+  const { loading, data, error } = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json",
+  });
+  console.log(loading, JSON.stringify(data), error);
   return <div className="App"></div>;
 };
 
---
+++ src/useAxios.js
@@ -0,0 +1,37 @@
+import defaultAxios from "axios";
+import { useState, useEffect } from "react";
+
+const useAxios = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null,
+  });
+
+  if (!opts.url) {
+    return;
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({
+          ...state,
+          loading: false,
+          error,
+        });
+      });
+  }, []);
+
+  return state;
+};
+
+export default useAxios;
